87bd44885fdeb653927f54361f62fd29c89799ee
Richard-Scap <scap222@gmail.com>
2016-07-10 20:50:30 -0500

    created user model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,24 +9,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Mock Twitter App"
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Mock Twitter App"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Mock Twitter App"
   end
 
     test "should get contact" do
     get contact_url
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Mock Twitter App"
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         FILL_IN
-    assert_equal full_title("Help"), FILL_IN
+    assert_equal full_title,         "Mock Twitter App"
+    assert_equal full_title("Help"), "Help | Mock Twitter App"
   end
 end
