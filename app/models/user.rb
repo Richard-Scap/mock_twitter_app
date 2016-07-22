aeab463ea95dc8421daa3bb98b19ee80e170a0b1
Richard-Scap <scap222@gmail.com>
2016-07-22 12:57:45 -0500

    completed advanced login (remember user with cookies)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
+
 	#validate name
 	validates :name,  presence: true, length: {maximum: 50}
 	#validate email using VALID_EMAIL_REGEX expression
@@ -12,10 +14,35 @@ class User < ApplicationRecord
 	validates :password, presence: true, length: { minimum: 6 }
 
 
-  # Returns the hash digest of the given string.
-  def User.digest(string)
+  # Returns the hash digest of the given string
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Creates randonly generated token
+  def self.new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  # Updates token to database for remembering current user session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    if remember_digest.nil?
+      false
+    else
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  end
+
+  # Forgets user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
